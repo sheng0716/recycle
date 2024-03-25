1deb51c33a7fcce1b9647c60ab8389bc44e82eca
goh <jieshenggoh@gmail.com>
2024-03-26 02:18:53 +0800

    center location coordinate can send to the map, then parse to float data type, <Marker> come out error when did not do an action to parse to float, but in database is real data type -sqlite. The marker could show already based on the center location

--- app/map.js
+++ app/map.js
@@ -8,28 +8,37 @@ import { useLocalSearchParams } from "expo-router";
 const map = () => {
     //this is utar location
     //3.0398872684355758, 101.79468236116317
-    const { latitude, longitude } = useLocalSearchParams();
-    console.log('latitude:', latitude, ' longitude', longitude);
+    const { latitude: stringLatitude, longitude: stringLongitude } = useLocalSearchParams();
+    console.log('String: latitude:', stringLatitude, ' longitude', stringLongitude);
+
+    const defaultLatitude = 34.0522; // Example default latitude
+    const defaultLongitude = -118.2437; // Example default longitude
+    const latitude = parseFloat(stringLatitude) || defaultLatitude;
+    const longitude = parseFloat(stringLongitude) || defaultLongitude;
+    console.log('Float: latitude:', latitude, ' longitude', longitude);
+
+    const [mapRegion, setMapRegion] = useState({
+        latitude: latitude,
+        longitude: longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    });
+    useEffect(() => {
+        if (latitude && longitude) { // Check if latitude and longitude are not null or undefined
+            setMapRegion((prevRegion) => ({
+                ...prevRegion,
+                latitude: latitude,
+                longitude: longitude,
+            }));
+        }
+    }, [latitude, longitude]);
     return (
         <View style={styles.container}>
-            <Text>123{latitude}</Text>
-            {/* <MapView
-                style={StyleSheet.absoluteFill} // Fill the entire screen with the map
-                initialRegion={{
-                    latitude: latitude,
-                    longitude: longitude,
-                    latitudeDelta: 0.0922, // Adjust as needed for zoom level
-                    longitudeDelta: 0.0421, // Adjust as needed for zoom level
-                }}
-            >
-                <Marker
-                    coordinate={{ latitude: latitude, longitude: longitude }}
-                    title={"Location Title"} // Optional
-                    description={"Location Description"} // Optional
-                />
-            </MapView> */}
-        </View>
+            <MapView style={styles.map} region={mapRegion}>
 
+                <Marker coordinate={{ latitude: latitude, longitude: longitude }} title="Target" />
+            </MapView>
+        </View >
     );
 };
 const styles = StyleSheet.create({
